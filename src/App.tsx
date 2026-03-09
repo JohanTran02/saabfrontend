c37ad8d84446a803a43c1362f0b6bd49d7f68c75
Gabriel Kassarp <gkassarp@gmail.com>
2026-03-09 19:28:11 +0100

    Setup chakra and imported example drawer

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+
 import Map from "./Map/Map"
+import DrawerUI from "./components/DrawerUI"
+import { Provider } from "./components/ui/provider"
 
 function App() {
   return (
+<Provider>
+  <DrawerUI/>
     <Map />
+</Provider>
   )
 }
 
