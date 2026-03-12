db9aa471ea55c4a2bcd5d020eaac39060f3f087a
JohanTran <johan.tran0210@gmail.com>
2026-03-12 15:31:06 +0100

    feat(base):Create zustand store for base

--- src/Map/BaseStore.ts
+++ src/Map/BaseStore.ts
@@ -1,20 +1,44 @@
 import { create } from "zustand";
-import type { Station } from "./types";
+import type { Flights, Resources, Base } from "./types";
+import { dummyStations } from "./constants";
+import { decreaseResources, increaseResources } from "./helpers";
 
 type BaseState = {
-    bases: Station[];
+    bases: Base[];
+    assignedFlights: Flights['id'][],
+    assignedResources: Resources,
+    currentResources: Resources
 }
 
 type BaseAction = {
-    addStation: (base: Station) => void
+    addBase: (base: Base) => void,
+    removeBase: (baseId: string) => void
+    updateBase: (updatedBase: Base) => void
+    addFlights: (flights: Flights['id'][]) => void
+    removeFlights: (flightIds: string[]) => void,
+    incrementCurrentResources: (resources: Resources) => void,
+    decreaseCurrentResources: (resources: Resources) => void,
+    incrementAssignedResources: (resources: Resources) => void,
+    decreaseAssignedResources: (resources: Resources) => void,
 }
 
 export const useBaseStore = create<BaseState & BaseAction>((set) => ({
-    bases: [
-        { stationType: 'huvudbas', coordinates: [60.073774388521656, 16.808927085708422], id: 'asdfoasjdfoajsodf' },
-        { stationType: 'reservbas', coordinates: [60.13882461404783, 14.123980863454367], id: 'iosajdgouiasdfiais' },
-        { stationType: 'sidobas', coordinates: [60.66805933137298, 14.070586699767079], id: 'asodfoasdjfoasdfjas' },
-        { stationType: 'reservbas', coordinates: [59.268549179577285, 14.266775673332363], id: 'aoisdfiuahsdiufhaiusdfgiuasd' },
-    ],
-    addStation: (base) => set((state) => ({ bases: [...state.bases, base] }))
+    bases: dummyStations,
+    assignedFlights: [],
+    assignedResources: {} as Resources,
+    currentResources: {} as Resources,
+    addBase: (base) => set((state) => ({ bases: [...state.bases, base] })),
+    removeBase: (baseId) => set((state) => ({ bases: state.bases.filter((stationId) => baseId !== stationId.id) })),
+    updateBase: (updatedBase: Base) =>
+        set((state) => ({
+            bases: state.bases.map((base) =>
+                base.id === updatedBase.id ? updatedBase : base
+            ),
+        })),
+    addFlights: (flights) => set((state) => ({ assignedFlights: [...state.assignedFlights, ...flights] })),
+    removeFlights: (flightIds) => set((state) => ({ assignedFlights: state.assignedFlights.filter((flight) => !flightIds.includes(flight)) })),
+    incrementCurrentResources: (resources) => set((state) => ({ currentResources: increaseResources(state.currentResources, resources) })),
+    decreaseCurrentResources: (resources) => set((state) => ({ currentResources: decreaseResources(state.currentResources, resources) })),
+    incrementAssignedResources: (resources) => set((state) => ({ assignedResources: increaseResources(state.assignedResources, resources) })),
+    decreaseAssignedResources: (resources) => set((state) => ({ assignedResources: decreaseResources(state.assignedResources, resources) }))
 }))
