7ba2e631db45c14a122add6ab0018b4803e57b0e
JohanTran <johan.tran0210@gmail.com>
2026-03-14 13:44:09 +0100

    feat(bases):Create fetch functions for bases

--- src/Map/fetches.ts
+++ src/Map/fetches.ts
@@ -20,3 +20,57 @@ export async function fetchAllBases() {
         console.error(error);
     }
 }
+
+export async function addBase(base: Base) {
+    try {
+        const response = await fetch('http://localhost:5051/create-air-base', {
+            method: "POST",
+            body: JSON.stringify({ base: base }),
+        });
+
+        console.log(response.status);
+    }
+    catch (error) {
+        console.error(error);
+    }
+}
+
+export async function deleteBase(baseId: string) {
+    try {
+        const response = await fetch(`http://localhost:5051/delete-air-base/${baseId}`, {
+            method: "DELETE"
+        });
+
+        console.log(response.status);
+    }
+    catch (error) {
+        console.error(error);
+    }
+}
+
+export async function getBase(baseId: string) {
+    try {
+        const response = await fetch(`http://localhost:5051/get-air-base/${baseId}`, {
+            method: "GET"
+        });
+
+        console.log(response.status);
+    }
+    catch (error) {
+        console.error(error);
+    }
+}
+
+export async function updateBase(base: Base | Base[]) {
+    try {
+        const response = await fetch(`http://localhost:5051/update-air-base`, {
+            method: "UPDATE",
+            body: JSON.stringify({ base: base })
+        });
+
+        console.log(response.status);
+    }
+    catch (error) {
+        console.error(error);
+    }
+}
