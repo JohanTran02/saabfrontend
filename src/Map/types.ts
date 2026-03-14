be38ac1aa5ca9cdea7371e3c78369bd59dcdd7c6
Gabriel Kassarp <145324695+GaKa00@users.noreply.github.com>
2026-03-14 12:42:36 +0100

    refactored new basetype

--- src/Map/types.ts
+++ src/Map/types.ts
@@ -1,35 +1,98 @@
 
 
-export type Base = {
-    id: string,
-    name: string,
-    baseType: BaseType,
-    coordinates: [lat: number, long: number],
-    assignedResources: ResourceItem[],
-    assignedFlights: Flights[];
-    currentResources: ResourceItem[];
+// export type Base = {
+//     id: string,
+//     name: string,
+//     baseType: BaseType,
+//     coordinates: [lat: number, long: number],
+//     assignedResources: ResourceItem[],
+//     assignedFlights: Flights[];
+//     currentResources: ResourceItem[];
+// }
+
+// type BaseResourceType = {
+//     amount: number,
+//     type: ResourceType,
+//     unit: string | null,
+// }
+// export type ResourceItem = Resources[ResourceType] & {
+//   id: string;
+//   name: string;
+// };
+
+//  export type ResourceType = 'fuel' | 'ammunition' | 'weapons' | 'spare_parts';
+
+// export type Resources = {
+//     [Key in ResourceType]: BaseResourceType;
+// }
+
+// export type Flights = {
+//     id: string,
+//     model: string,
+//     status: 'idle' | 'flying' | 'repairing'
+// }
+
+// export type BaseType = 'huvudbas' | 'sidobas' | 'reservbas';
+
+
+
+export interface Coordinate {
+    latitude: number;
+    longitude: number;
 }
 
-type BaseResourceType = {
-    amount: number,
-    type: ResourceType,
-    unit: string | null,
-}
-export type ResourceItem = Resources[ResourceType] & {
-  id: string;
-  name: string;
-};
-
- export type ResourceType = 'fuel' | 'ammunition' | 'weapons' | 'spare_parts';
-
-export type Resources = {
-    [Key in ResourceType]: BaseResourceType;
+export interface Status {
+    id: number;
+    statusType: string;
 }
 
-export type Flights = {
-    id: string,
-    model: string,
-    status: 'idle' | 'flying' | 'repairing'
+export interface PermanenceType {
+    id: number;
+    permanence: string;
 }
 
-export type BaseType = 'huvudbas' | 'sidobas' | 'reservbas';
+export interface CargoItem {
+    id: string; 
+    title: string;
+    quantity: number;
+    unit: string;
+    note: string | null;
+}
+
+export interface OnGoingRepair {
+    id: number;
+    maintenanceType: {
+        id: number;
+        title: string;
+        description: string;
+        estimatedCompletion?: number; 
+    };
+}
+
+export interface Schedule {
+    id: number;
+    destination: Coordinate;
+    scheduledDate: string | Date; 
+    createdDate: string | Date;
+}
+
+export interface Vehicle {
+    guid: string;
+    identifier: string;
+    model: string;
+    position: Coordinate;
+    status: Status | null;
+    scheduleNavigation: Schedule | null;
+    onGoingRepairs: OnGoingRepair[];
+    cargoItems: CargoItem[];
+}
+
+export interface Base {
+    guid: string;          
+    name: string;
+    position: Coordinate;
+    statusNavigation: Status;
+    permanenceNavigation: PermanenceType;
+    vehiclesStationed: Vehicle[];
+    cargoItems: CargoItem[]; 
+}
