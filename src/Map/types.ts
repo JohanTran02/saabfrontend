4e66205cc3c7cb1b526b75cdd72a825a6954226b
Gabriel Kassarp <145324695+GaKa00@users.noreply.github.com>
2026-03-14 12:59:52 +0100

    removed comment on old base

--- src/Map/types.ts
+++ src/Map/types.ts
@@ -1,25 +1,4 @@
 
-
-// export type Base = {
-//     id: string,
-//     name: string,
-//     baseType: BaseType,
-//     coordinates: [lat: number, long: number],
-//     assignedResources: ResourceItem[],
-//     assignedFlights: Flights[];
-//     currentResources: ResourceItem[];
-// }
-
-// type BaseResourceType = {
-//     amount: number,
-//     type: ResourceType,
-//     unit: string | null,
-// }
-// export type ResourceItem = Resources[ResourceType] & {
-//   id: string;
-//   name: string;
-// };
-
 export type ResourceType = 'fuel' | 'ammunition' | 'weapons' | 'spare_parts';
 
 export type BaseResourceType = {
